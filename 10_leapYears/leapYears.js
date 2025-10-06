487bed7b3ca53467dc5ad0b6f9d1d9b728f94bf2
Robby Jeunen <robbyjeunen@gmail.com>
2025-10-06 14:54:52 +0200

    solved excercise 10 leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(year / 4 % 0 && year / 100 % 0 && year / 400 % 0){
+        return true;
+    }
 
+    if(year % 4 == 0){
+        if(year % 400  == 0){
+            return true;
+        }
+        else if(year % 100 == 0 && year % 400 != 0){
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+//als jaar gedeeld door 4 100 en 400 - leapYear
