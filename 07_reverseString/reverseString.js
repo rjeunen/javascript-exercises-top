355e8f4de6490d5f7bc39b2231fb2dc9444810a2
Robby Jeunen <robbyjeunen@gmail.com>
2025-10-06 14:04:06 +0200

    solved excercise 07 revrsedString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedWord = "";
+    for(let i = word.length; i >= 0; i--){
+        reversedWord += word.charAt(i);
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
